7b47668c596812e49bb672aa16e2891d558def9c
L0NEBIRD <raminlnwza555@gmail.com>
2023-09-22 15:51:53 +0700

    Change bloodgroup.c to fix commit error

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-<<<<<<< HEAD
-{ char Name[80], G[90], answer[10];
-=======
-{ char Name[30], G[10], answer[10];
->>>>>>> 09769379e8f519e2650291dd04a2fa2409a38208
-  int i;
-  do
-  {
-    printf("What is your name? ");
-    scanf("%s", Name);
+{
+    char Name[30], G[10], answer[10];
+    int i;
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
-      scanf("%s", G);
+        printf("What is your name? ");
+        scanf("%s", Name);
+        do
+        {
+            printf("What is your blood group [A, B, AB, or O]? ");
+            scanf("%s", G);
 
-      if (strcmp(G, "A") !=0  &&
-                            strcmp(G, "B") !=0  &&
-                            strcmp(G, "AB") !=0   &&
-                            strcmp(G, "O") !=0 )
-        printf("Blood group %s is incorrect! Please try again.\n", G);
-    }  while (strcmp(G, "A") !=0  &&
-                            strcmp(G, "B") !=0  &&
-                            strcmp(G, "AB") !=0   &&
-                            strcmp(G, "O") !=0 );
-    if (strcmp(G,"A") ==0)
-    {
-      printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
-      printf("  You can receive blood from: A, O.\n");
+            if (strcmp(G, "A") !=0  &&
+                    strcmp(G, "B") !=0  &&
+                    strcmp(G, "AB") !=0   &&
+                    strcmp(G, "O") !=0 )
+                printf("Blood group %s is incorrect! Please try again.\n", G);
+        }
+        while (strcmp(G, "A") !=0  &&
+                strcmp(G, "B") !=0  &&
+                strcmp(G, "AB") !=0   &&
+                strcmp(G, "O") !=0 );
+        if (strcmp(G,"A") ==0)
+        {
+            printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
+            printf("  You can receive blood from: A, O.\n");
+        }
+        else if (strcmp(G, "B") ==0)
+        {
+            printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
+            printf("  You can receive blood from: B, O.\n");
+        }
+        else if (strcmp(G, "AB") == 0)
+        {
+            printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
+            printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
+        }
+        else
+        {
+            printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
+            printf("  But Sad! You can receive blood only from: O\n");
+        }
+        printf("\nContinue (YES for Yes)? ");
+        scanf("%s", answer);
     }
-    else if (strcmp(G, "B") ==0)
-    {
-      printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
-      printf("  You can receive blood from: B, O.\n");
-    }
-    else if (strcmp(G, "AB") == 0)
-    {
-      printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
-      printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
-    }
-    else
-    {
-      printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
-      printf("  But Sad! You can receive blood only from: O\n");
-    }
-    printf("\nContinue (YES for Yes)? ");
-    scanf("%s", answer);
-  } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+    while (strcmp(answer, "YES") == 0);
+    printf("Goodbye\n");
 }
